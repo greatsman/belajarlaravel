30e9f31dcf145bbd329703dd968954e3208584aa
Agung Septyanto Putra <agungseptyantoputra@gmail.com>
2019-01-10 17:13:38 +0700

    first commit

---
+++ routes/web.php
@@ -0,0 +1,31 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::get('halo', function () {
+	return "Halo, Selamat datang di tutorial laravel www.malasngoding.com";
+});
+Route::get('blog', function () {
+	return view('blog');
+});
+Route::get('dosen', 'DosenController@index');
+Route::get('/pegawai/{nama}', 'PegawaiController@index');
+Route::get('/formulir', 'PegawaiController@formulir');
+
+Route::post('/formulir/proses', 'PegawaiController@proses');
+
+// route blog
+Route::get('/blog', 'BlogController@home');
+Route::get('/blog/tentang', 'BlogController@tentang');
+Route::get('/blog/kontak', 'BlogController@kontak');
